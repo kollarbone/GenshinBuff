065616f631760bbc05a5a44ffa2a32a64edab6a5
kollarbone <106979919+kollarbone@users.noreply.github.com>
2022-11-01 17:44:15 +0300

    туц

--- src/theme.js
+++ src/theme.js
@@ -14,188 +14,249 @@ export const GlobalStyles = createGlobalStyle`
   }
   .header {
     background-color: ${({ theme }) => theme.body};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .logo_1 {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .logo_2 {
     color: ${({ theme }) => theme.text_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .button_back {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .button_back:hover {
     color: ${({ theme }) => theme.text_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .item {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .item.active {
     border-bottom: 2px solid ${({ theme }) => theme.text_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .current_events {
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .current_banners {
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .current_talents {
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .current_news {
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .our_social_networks {
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .main_content {
     background-color: ${({ theme }) => theme.body};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .banner_corusel {
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .calendarIcon {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .heading_1 {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .heading_2 {
     color: ${({ theme }) => theme.text_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .headings { 
     border-bottom: 1px solid ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .image_talent_character {
     background-color: ${({ theme }) => theme.body};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .talents {
     border-top: 1px solid ${({ theme }) => theme.border};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .talents_all {
     color: ${({ theme }) => theme.border};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .new {
     background-color: ${({ theme }) => theme.body};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .news_name {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .news_description {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .Characters {
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .characters_header {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .input {
     background-color: ${({ theme }) => theme.body};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .character_element {
     background-color: ${({ theme }) => theme.body};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .characters {
     background-color: ${({ theme }) => theme.body};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .character_name {
     color: ${({ theme }) => theme.text};
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .character_card_info {
     background-color: ${({ theme }) => theme.body};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .Character {
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .skills_and_attack {
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .bilds_character {
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .character_prev_img {
     border: 1px solid ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .name_main_info {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .value_main_info {
     color: ${({ theme }) => theme.text_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .character_titul {
     color: ${({ theme }) => theme.text_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .character_image_info {
     background-color: ${({ theme }) => theme.body};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .character_about {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .combat_info {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .character_combat_info {
     background-color: ${({ theme }) => theme.body};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .attack {
     background-color: ${({ theme }) => theme.body};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .attack_name {
     color: ${({ theme }) => theme.text_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .attack_key {
     color: ${({ theme }) => theme.border};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .attack_value {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .constellation_character_dis {
     background-color: ${({ theme }) => theme.body};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .image_character_bild {
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .character_bild_bonus {
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .bild_name {
     color: ${({ theme }) => theme.text_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .heading_3 {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .heading_4 {
     color: ${({ theme }) => theme.text_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .heading_5 {
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .bild {
     background-color: ${({ theme }) => theme.body};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .artif_image svg{
     color: ${({ theme }) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .TierList {
     background-color: ${({ theme }) => theme.body_2};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .heading_6 {
     color: ${({ theme }) => theme.border};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   .heading_7 {
     color: ${({ theme }) => theme.border};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
   }
   @media only screen and (max-width: 768px) {
     .item.active {
       color: ${({ theme }) => theme.text_2};
+      transition: background 0.2s ease-in, color 0.2s ease-in;
     }
     .burger_bar {
       background-color: ${({ theme }) => theme.text};
+      transition: background 0.2s ease-in, color 0.2s ease-in;
     }
     .burger__dark .menu {
       background-color: ${({ theme }) => theme.body};
+      transition: background 0.2s ease-in, color 0.2s ease-in;
     }
   }
 `;
